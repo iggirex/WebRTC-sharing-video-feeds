2adacac53ab5801ddd54c25f264579eb102bfe58
Iggi <iggirex@gmail.com>
2016-09-25 11:50:38 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+//NODE EXPRESS SERVER///
+var express = require('express');
+var app = express();
+console.log('server started');
+
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
+app.listen(3000);
+/////////////////
