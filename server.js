05f44c21fbc1edfecfff93b48892c3dbb0f2c1a3
Iggi <iggirex@gmail.com>
2016-09-25 23:11:12 -0600

    changing camera settings

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 console.log('server started');
 
+//Linking to public folder in order to get CSS file
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
